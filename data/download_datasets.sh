ce4598d45f8b37061431aba4f8e1b500b0393aa5
Julie Fang <julie.fang@merckgroup.com>
2021-05-12 14:41:30 +0200

    add cervical dataset

--- data/download_datasets.sh
+++ data/download_datasets.sh
@@ -20,6 +20,8 @@ if [ "$1" = "adult" ]; then
     kaggle datasets download wenruliu/adult-income-dataset
     unzip adult-income-dataset.zip
     rm adult-income-dataset.zip
+elif [ "$1" = "cervical" ]; then
+    wget https://archive.ics.uci.edu/ml/machine-learning-databases/00383/risk_factors_cervical_cancer.csv
 elif [ "$1" = "nhanes" ]; then
     wget https://raw.githubusercontent.com/semerj/NHANES-diabetes/master/data/diabetes_data_train.csv
     wget https://raw.githubusercontent.com/semerj/NHANES-diabetes/master/data/diabetes_data_test.csv
